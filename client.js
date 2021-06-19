59dc0dca612811bfecf821909713c75468482487
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-06-18 22:09:57 -0400

    getting started

---
+++ client.js
@@ -0,0 +1,24 @@
+$(document).ready(onReady);
+
+function onReady() {
+    console.log(" This is going to work!")
+}
+const employees =[
+    {
+        firstName,
+        lastName,
+        iD,
+        title,
+        annualSalary,
+
+    },
+];
+
+function employeeAdd(employees) {
+    this.firstName = employee.firstName;
+    this.lastName = employee.lastName;
+    this.id = employee.id;
+    this.title = employee.title;
+    this.annualSalary = employee.annualSalary;
+    return employee
+}
