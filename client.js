b192b8332546182a13ad90335921203b5ce05ae4
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-06-20 18:59:51 -0400

    cost and table almost done

--- client.js
+++ client.js
@@ -4,6 +4,7 @@ function onReady() {
     console.log(" This is going to work!")
 
     $('#employeeAddSubmit').on('click', employeeAdd);
+    addTable(people);
 }
 
 const employees = [];
@@ -31,17 +32,30 @@ $('#title').val('');
 $('#salary').val('');
 
 }    
+// add employee is done
+
+function addTable() {
+    $('person').prepend(`
+    <tr id="person">
+        <td class="tableFirstName">First Name</td>
+        <td class="tableLastName">Last Name</td>
+        <td class="tableID">ID</td>
+        <td class="tableTitle">Title</td>
+        <td class="tableSalary">Salary</td>
+    </tr>
+    `);
+}
 
 //time to add the employee cost up
 function total() {
     let totalSalary = 0;  // have to add up and divide by 12 months
     for (let i = 0; i < employees.length; i++) {
-        const employeeSalary = employees[i][annualSalary];
-        totalSalary += totalSalary;
+        const employeeSalary = employees[i]['salary'];
+        totalSalary += employeeSalary;
 
     }
     totalSalary = totalSalary / 12;
-    $('#monthlyCost').html('Cost Monthly:' + (totalSalary).toLocaleString;
+    $('#monthlyCost').html('Cost Monthly:' + (totalSalary).toLocaleString('en-us', {style:'currency', currency: 'USD',}));
     console.log(totalSalary);
     $( '#totalSalaryText' ).text(totalSalary);
 }
